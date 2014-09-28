ba6c0fd1edd1c418600488f0c7953fc077ec078e
DevRoMc <devrosemberg@devrosemberg.com>
2014-09-28 17:29:38 -0300

    Let's get this bby started.

--- forum/index.php
+++ forum/index.php
@@ -1,6 +1,8 @@
 <?php
-	include("config.php");
+	// Loading config 
+	include("config/config.php");
 
+	// If the forum is installed you will be able to see it, if not it will load the setup
 	if (FORUM_INSTALLED == true) {
 		include_once("viewforum.php");
 	}
