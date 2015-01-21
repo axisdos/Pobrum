408e1c4e7eb7a78633026d94858862cf551b99aa
mattrick <matthewhighrollers@gmail.com>
2015-01-20 18:19:08 -0800

    Add comments

--- forum/database/database.mysql.php
+++ forum/database/database.mysql.php
@@ -8,6 +8,22 @@ class Database {
 
 	public static $con = null;
 
+	//Connect to default database
+	public static function connect() {
+		$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_SCHEMA);
+		if ($mysqli->connect_errno) {
+			if (DEBUG) {
+				echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			}
+			return false;
+		}
+		else {
+			self::$con = $mysqli;
+			return true;
+		}
+	}
+
+	//Connect to other database
 	public static function connect($host, $user, $password, $database) {
 		$mysqli = new mysqli($host, $user, $password, $database);
 		if ($mysqli->connect_errno) {
@@ -22,9 +38,12 @@ class Database {
 		}
 	}
 
+	//Disconnect from current database
 	public static function disconnect() {
 		return self::$con->close();
 	}
+
+	//TODO Fix query functions, help needed
 
 	public static function query($statement, $arguments) {
 		if (!($stmt = self::$con->prepare($statement))) {
@@ -40,7 +59,7 @@ class Database {
 	}
 
 	public static function update($statement, $arguments) {
-		$stmt = self::$con->prepare($statement);
+		$stmt = self::$con->prepare($statement));
 
 		print_r($arguments);
 		foreach ($arguments as $arg) {
